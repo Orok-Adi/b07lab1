4c5f24e0092d138b63f0c71a1252664ba76a152d
Orok-Adi <lilliedeltag@gmail.com>
2025-09-28 23:11:53 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,17 +1,43 @@
-
-public class Driver {
-public static void main(String [] args) {
-Polynomial p = new Polynomial();
-System.out.println(p.evaluate(3));
-double [] c1 = {6,0,0,5};
-Polynomial p1 = new Polynomial(c1);
-double [] c2 = {0,-2,0,0,-9};
-Polynomial p2 = new Polynomial(c2);
-Polynomial s = p1.add(p2);
-System.out.println("s(0.1) = " + s.evaluate(0.1));
-if(s.hasRoot(1))
-System.out.println("1 is a root of s");
-else
-System.out.println("1 is not a root of s");
-}
+import java.io.*;
+    public class Driver {
+	public static void main(String [] args) throws IOException{
+	Polynomial p = new Polynomial();
+	System.out.println(p.evaluate(3));
+	double [] c1 = {6,5};
+	int [] d1 = {0, 3};
+	Polynomial p1 = new Polynomial(c1, d1);
+	double [] c2 = {-2,-9};
+	int [] d2 = {1,4};
+	Polynomial p2 = new Polynomial(c2, d2);
+	Polynomial s = p1.add(p2);
+	File file1 = new File("file1.txt");
+	Polynomial q = new Polynomial(file1);
+	s.saveToFile("file2");
+	System.out.println("There should be a file named file2 with the polynomial expression of polynomial s");
+	System.out.println("Below are the three coefficients of polynomial q taken from file1");
+	System.out.println(q.x[0]);
+	System.out.println(q.x[1]);
+	System.out.println("Below are the three powers of each coefficient of polynomial q taken from file1");
+	System.out.println(q.y[0]);
+	System.out.println(q.y[1]);   
+	System.out.println("s(0.1) = " + s.evaluate(0.1));
+	if(s.hasRoot(1)){
+	    System.out.println("1 is a root of s");
+	}       
+	else{
+	    System.out.println("1 is not a root of s");
+	}
+	Polynomial r = q.multiply(p2);
+	System.out.println("Below are the 6 coefficients of polynomial r made by multiplying polynomial q with polynomial p2");
+	System.out.println(r.x[0]);
+	System.out.println(r.x[1]);
+	System.out.println(r.x[2]);
+	System.out.println(r.x[3]);
+	System.out.println("Below are the 6 powers of each coefficient of polynomial r made by multiplying polynomial q with polynomial p2");
+	System.out.println(r.y[0]);
+	System.out.println(r.y[1]);
+	System.out.println(r.y[2]);
+	System.out.println(r.y[3]);
+	System.out.println("r(0.1) = " + r.evaluate(0.1));
+    }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -2,46 +2,97 @@ import java.io.*;
 public class Polynomial{
 
     double [] x;
+    int [] y;
     
     public Polynomial(){
     
 	x = new double []{0};
+	y = new int []{1};
 	
     }
-    public Polynomial(double [] input){
+    public Polynomial(double [] input1, int [] input2){
     
-	x = (double[]) input.clone();
+	x = (double[]) input1.clone();
+	y = (int[]) input2.clone();
 	
+    }
+    public Polynomial(File file1) throws IOException{
+	BufferedReader input = new BufferedReader(new FileReader(file1));
+	String expression = input.readLine();
+	
+	expression = expression.replaceAll("-", "+-");
+	
+	if(expression.charAt(0) == '+'){
+	    expression = expression.substring(1);
+	}
+	
+	String [] expressionarray = expression.split("[+]");
+	
+	int expressionlength = expressionarray.length;
+	
+	double [] coeff = new double [expressionlength];
+	int [] power = new int [expressionlength];
+	
+	String [] expressionterm;
+	
+	for (int i = 0; i < expressionlength; i++){
+	    expressionterm = expressionarray[i].split("[x]");
+	    if (expressionterm.length == 2){
+		coeff[i] = Double.parseDouble(expressionterm[0]);
+		power[i] = Integer.parseInt(expressionterm[1]);
+	    }
+	    else{
+		coeff[i] = Double.parseDouble(expressionterm[0]);
+		power[i] = 0;
+	    }
+	}
+	
+	x = (double[]) coeff.clone();
+	y = (int[]) power.clone();
     }
     public Polynomial add(Polynomial express2){
-	
-	int size;
     
-	if (express2.x.length > x.length){
-	    size = x.length;
-	    for(int i = 0; i < size; i++){
-		express2.x[i] = express2.x[i] + x[i];
+	double [] x2 = (double[]) express2.x.clone();
+    
+	int size = express2.x.length + x.length;
+
+	for(int i = 0; i < express2.x.length; i++){
+	    for(int j = 0; j < x.length; j++){
+		if (y[j] == express2.y[i]){
+		    x[j] += x2[i];
+		    x2[i] = 0;
+		    size--;
+		    break;
+		}
 	    }
 	}
-	else{
-	    size = express2.x.length;
-	    for(int i = 0; i < size; i++){
-		x[i] = express2.x[i] + x[i];
-	    }
+	double [] newx = new double [size];
+	int [] newy = new int [size];
+	for(int i = 0; i < x.length; i++){
+	    newx[i] = x[i];
+	    newy[i] = y[i];
 	}
 	
-	if (express2.x.length > x.length){
-	    return express2;
+	int count = 0;
+	
+	for(int i = 0; i < x2.length; i++){
+	    if(x2[i] != 0){
+		newx[x.length + count] = x2[i];
+		newy[x.length + count] = express2.y[i];
+		count++;
+	    }
 	}
-	express2.x = x;
-	return express2;
+
+	Polynomial result = new Polynomial(newx, newy);
+
+	return result;
     }
     public double evaluate(double xval){
 	
 	double sum = 0;
 	
 	for(int i = 0; i < x.length; i++){
-	    sum += x[i]*(Math.pow(xval, i));
+	    sum += x[i]*(Math.pow(xval, y[i]));
 	}
 	return sum;
     }
@@ -51,8 +102,63 @@ public class Polynomial{
 	}
 	return false;
     }
-    public static void main (String args[]){
+    public Polynomial multiply(Polynomial express2){
+	Polynomial sumpoly = new Polynomial();
+	double [] coeff = new double [x.length];
+	int [] power = new int [x.length];
+	for(int i = 0; i < express2.x.length; i++){
+	    for(int j = 0; j < x.length; j++){
+		coeff[j] = express2.x[i]*x[j];
+		power[j] = express2.y[i]+y[j];
+	    }
+	    if(i == 0){
+		sumpoly = new Polynomial(coeff, power);
+	    }
+	    else{
+		sumpoly = sumpoly.add(new Polynomial(coeff, power));
+	    }                       
+	}
+	return sumpoly;
+    }
+    public void saveToFile(String filename) throws IOException{
+	String expression = "";
 	
+	File file1 = new File(filename + ".txt");
+	
+	file1.createNewFile();
+	
+	for (int i = 0;i < x.length; i++){
+	    if (y[i] == 0){
+		if (x[i] < 0){
+		    expression = expression + (int)x[i] ;
+		}
+		else{
+		    expression = expression + "+" + (int)x[i];
+		}               
+	    }
+	    else if (y[i] == 1){
+		 if (x[i] < 0){
+		    expression = expression + (int)x[i] + "x";
+		}
+		else{
+		    expression = expression + "+" + (int)x[i] + "x";
+		}               
+	    }
+	    else{
+		if (x[i] < 0){
+		    expression = expression + (int)x[i] + "x" + y[i];
+		}
+		else{
+		    expression = expression + "+" + (int)x[i] + "x" + y[i];
+		} 
+	    }
+	}
+	expression = expression.substring(1);
+	FileWriter writer = new FileWriter(file1);
+	writer.write(expression);
+	writer.close();
+    }
+    public static void main (String args[]){
 	
     }
 }
